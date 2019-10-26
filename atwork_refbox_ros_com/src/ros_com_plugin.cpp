4f86bb0efba325b7df96367d17aaeae4188e3810
Christoph Steup <steup@ivs.cs.ovgu.de>
2019-10-26 11:54:59 +0200

    Fix name changes of packages

--- atwork_refbox_ros_com/src/ros_com_plugin.cpp
+++ atwork_refbox_ros_com/src/ros_com_plugin.cpp
@@ -16,7 +16,7 @@ public:
     RosCom() {}
     virtual ~RosCom() {}
 
-    virtual void sendTask( atwork_refbox_msgs::Task task ) {
+    virtual void sendTask( atwork_refbox_ros_msgs::Task task ) {
         this->send_task_pub.publish( task );
     }
 
@@ -24,11 +24,11 @@ public:
         this->rh = roshandle;
 
         this->robot_state_sub = this->rh.subscribe( "/refbox/robot_state", 10, &RosCom::receiveRobotStateClb, this );
-    	this->send_task_pub = this->rh.advertise<atwork_refbox_msgs::Task>("/refbox/task", 1);
+    	this->send_task_pub = this->rh.advertise<atwork_refbox_ros_msgs::Task>("/refbox/task", 1);
     }
 
 private:
-    void receiveRobotStateClb( const atwork_refbox_msgs::RobotState::ConstPtr& msg ) {
+    void receiveRobotStateClb( const atwork_refbox_ros_msgs::RobotState::ConstPtr& msg ) {
     	this->sendRobotState( *msg );
     }
 
