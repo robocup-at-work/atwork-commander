c239122285b66b66693e4f8e6319f822e43cae37
Martin <maseidel@ovgu.de>
2019-11-14 20:24:37 +0100

    Read plugin list from ROS param server

--- atwork_refbox_ros_com/src/communication_node.cpp
+++ atwork_refbox_ros_com/src/communication_node.cpp
@@ -1,50 +1,56 @@
 #include "atwork_refbox_com/plugin_interface.h"
 #include <pluginlib/class_loader.h>
 
-ros::Publisher robot_state_pub;
+using if_t = atwork_refbox_ros::communication::Interface;
 
-boost::shared_ptr<atwork_refbox_ros::communication::Interface> plugin;
+ros::Publisher g_robot_state_pub;
+std::vector< boost::shared_ptr< if_t > > g_plugins;
 
-
-void sendTaskClb( const atwork_refbox_ros_msgs::Task::ConstPtr& msg ) {
-	plugin->sendTask( *msg );
-}
-
-void receiveRobotState( atwork_refbox_ros_msgs::RobotState robot_state ) {
-	ROS_INFO_STREAM("new robot_state: " << robot_state );
-	robot_state_pub.publish( robot_state );
-}
-
-int main(int argc, char **argv)
+void sendTaskClb(const atwork_refbox_ros_msgs::Task::ConstPtr& msg)
 {
-	ros::init(argc, argv, "atwork_refbox_com_node");
-	ros::NodeHandle roshandle;
-
-	ros::Subscriber send_task_sub = roshandle.subscribe( "/refbox/internal/task", 1, &sendTaskClb );
-	robot_state_pub = roshandle.advertise<atwork_refbox_ros_msgs::RobotState>("/refbox/internal/robot_state", 10);
-
-	//TODO: list, no default
-	std::string plugin_name;
-	//the plugin_name should include the namespace
-	ros::param::param<std::string>("~plugin", plugin_name, "atwork_refbox_ros::communication::RosCom");
-
-	pluginlib::ClassLoader<atwork_refbox_ros::communication::Interface> plug_in_loader( "atwork_refbox_ros_com", "atwork_refbox_ros::communication::Interface" );
-
-	ROS_INFO("[main] starting plugin \"%s\"", plugin_name.c_str());
-	try
-	{
-		plugin.reset();
-		plugin = plug_in_loader.createInstance(plugin_name.c_str());
-		plugin->initialize(roshandle, std::bind( &receiveRobotState, std::placeholders::_1 ));
-	} catch(pluginlib::PluginlibException& ex) {
-		ROS_FATAL("[main] failed to load. Error: \"%s\"\nGoodbye.", ex.what());
-		return 1;
+	for ( auto& p : g_plugins ) {
+    	p->sendTask(*msg);
 	}
-	ROS_INFO("[main] module loaded");
+}
 
-	ros::spin();
+void receiveRobotState(atwork_refbox_ros_msgs::RobotState robot_state)
+{
+    ROS_INFO_STREAM("new robot_state: " << robot_state);
+    g_robot_state_pub.publish(robot_state);
+}
 
-	plugin.reset();
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "atwork_refbox_com_node");
+    ros::NodeHandle roshandle;
 
-	return 0;
+    ros::Subscriber send_task_sub = roshandle.subscribe("/refbox/internal/task", 1, &sendTaskClb);
+    g_robot_state_pub = roshandle.advertise<atwork_refbox_ros_msgs::RobotState>("/refbox/internal/robot_state", 10);
+
+    std::vector<std::string> plugins_param;
+    ros::param::get("~plugins", plugins_param);
+
+	pluginlib::ClassLoader< if_t > plug_in_loader("atwork_refbox_ros_com", "atwork_refbox_ros::communication::Interface");
+
+	ROS_INFO_STREAM( plugins_param.size());
+	for ( auto p : plugins_param ) {
+		try {
+	        g_plugins.emplace_back( plug_in_loader.createInstance(p.c_str()) );
+	        g_plugins.back()->initialize(roshandle, std::bind(&receiveRobotState, std::placeholders::_1));
+			ROS_INFO("[main] starting plugin \"%s\"", p.c_str());
+	    } catch (pluginlib::PluginlibException& ex) {
+	        ROS_FATAL("[main] failed to load plugin \"%s\". Error:\n\"%s\"\nGoodbye.", p.c_str(), ex.what());
+	    }
+	}
+
+    ROS_INFO("[main] plugin loading finished");
+
+    ros::spin();
+
+	for ( auto& p : g_plugins ) {
+		std::cout << p.use_count();
+    	p.reset();
+	}
+
+    return 0;
 }
