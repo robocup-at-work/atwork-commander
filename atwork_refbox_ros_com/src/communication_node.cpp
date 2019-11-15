2ab7fe9c29cf7a5523a471d9cfc001ba7d29beba
Martin <maseidel@ovgu.de>
2019-11-15 17:47:30 +0100

    Improve communication node

--- atwork_refbox_ros_com/src/communication_node.cpp
+++ atwork_refbox_ros_com/src/communication_node.cpp
@@ -4,18 +4,18 @@
 using if_t = atwork_refbox_ros::communication::Interface;
 
 ros::Publisher g_robot_state_pub;
-std::vector< boost::shared_ptr< if_t > > g_plugins;
+std::vector<boost::shared_ptr<if_t>> g_plugins;
 
 void sendTaskClb(const atwork_refbox_ros_msgs::Task::ConstPtr& msg)
 {
-	for ( auto& p : g_plugins ) {
-    	p->sendTask(*msg);
-	}
+    for (auto& p : g_plugins) {
+        p->sendTask(*msg);
+    }
 }
 
 void receiveRobotState(atwork_refbox_ros_msgs::RobotState robot_state)
 {
-    ROS_INFO_STREAM("new robot_state: " << robot_state);
+    // ROS_INFO_STREAM("new robot_state: " << robot_state);
     g_robot_state_pub.publish(robot_state);
 }
 
@@ -30,27 +30,31 @@ int main(int argc, char** argv)
     std::vector<std::string> plugins_param;
     ros::param::get("~plugins", plugins_param);
 
-	pluginlib::ClassLoader< if_t > plug_in_loader("atwork_refbox_ros_com", "atwork_refbox_ros::communication::Interface");
+    pluginlib::ClassLoader<if_t> plug_in_loader("atwork_refbox_ros_com", "atwork_refbox_ros::communication::Interface");
 
-	ROS_INFO_STREAM( plugins_param.size());
-	for ( auto p : plugins_param ) {
-		try {
-	        g_plugins.emplace_back( plug_in_loader.createInstance(p.c_str()) );
-	        g_plugins.back()->initialize(roshandle, std::bind(&receiveRobotState, std::placeholders::_1));
-			ROS_INFO("[main] starting plugin \"%s\"", p.c_str());
-	    } catch (pluginlib::PluginlibException& ex) {
-	        ROS_FATAL("[main] failed to load plugin \"%s\". Error:\n\"%s\"\nGoodbye.", p.c_str(), ex.what());
-	    }
-	}
+    for (auto p : plugins_param) {
+        try {
+            g_plugins.emplace_back(plug_in_loader.createInstance(p.c_str()));
+            g_plugins.back()->initialize(roshandle, std::bind(&receiveRobotState, std::placeholders::_1));
+            ROS_INFO("[main|com] successfully initialized plugin \"%s\"", p.c_str());
+        } catch (pluginlib::PluginlibException& ex) {
+            ROS_FATAL("[main|com] failed to load plugin \"%s\". Error:\n\"%s\"", p.c_str(), ex.what());
+        }
+    }
 
-    ROS_INFO("[main] plugin loading finished");
+    if (g_plugins.size() > 0) {
+        ROS_INFO("[main|com] plugin loading finished");
+    } else {
+        ROS_FATAL("[main|com] no communication plugin was loaded. Exiting");
+        return 1;
+    }
 
     ros::spin();
 
-	for ( auto& p : g_plugins ) {
-		std::cout << p.use_count();
-    	p.reset();
-	}
+    for (auto& p : g_plugins) {
+        std::cout << p.use_count();
+        p.reset();
+    }
 
     return 0;
 }
