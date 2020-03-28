6313d3a64fbdceb9edf27cecf2774504c918ef2e
Christoph Steup <steup@ivs.cs.ovgu.de>
2020-03-28 19:51:09 +0100

    COM: update robot_example
            - Enable configuration of refbox namespace, team name and robot
            name

--- atwork_commander_com/src/robot_example.cpp
+++ atwork_commander_com/src/robot_example.cpp
@@ -3,33 +3,45 @@
 #include "atwork_commander_msgs/RobotState.h"
 #include "atwork_commander_msgs/Task.h"
 
-atwork_commander_msgs::Task g_cur_task;
-atwork_commander_msgs::RobotState g_robot_state;
+#include <string>
+
 
 void taskClb(const atwork_commander_msgs::Task::ConstPtr& msg)
 {
-    g_cur_task = *msg;
+    ROS_INFO_STREAM_NAMED("example", "[REFBOX_EXAMPLE] Got current task:" << std::endl << *msg );
 }
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "example_fake_robot");
-    ros::NodeHandle roshandle;
+    ros::NodeHandle nh("~");
 
-    ros::Subscriber task_sub = roshandle.subscribe("/refbox/task", 1, &taskClb);
-    ros::Publisher robot_state_pub = roshandle.advertise<atwork_commander_msgs::RobotState>("/refbox/robot_state", 1);
+    std::string refboxName = "atwork_commander";
+    std::string teamName = "exampleTeam";
+    std::string robotName = "exampleRobot";
 
-    g_robot_state.sender.team_name = "robOTTO";
-    g_robot_state.sender.robot_name = "Euler";
+    if( !nh.getParam("refboxName", refboxName) )
+      ROS_WARN_STREAM_NAMED("example", "[REFBOX-EXAMPLE] No refbox name configured using default: \"atwork_commander\"!");
+    if( !nh.getParam("teamName", teamName) )
+      ROS_WARN_STREAM_NAMED("example", "[REFBOX-EXAMPLE] No team name configured using default: \"exampleTeam\"!");
+    if( !nh.getParam("robotName", robotName) )
+      ROS_WARN_STREAM_NAMED("example", "[REFBOX-EXAMPLE] No robot name configured using default: \"exampleRobot\"!");
+
+    ros::Subscriber task_sub = nh.subscribe(std::string("/")+refboxName+"/task", 1, &taskClb);
+    ros::Publisher robot_state_pub = nh.advertise<atwork_commander_msgs::RobotState>(std::string("/")+refboxName+"/robot_state", 1);
+
+    atwork_commander_msgs::RobotState robot_state;
+    robot_state.sender.team_name = teamName;
+    robot_state.sender.robot_name = robotName;
 
     ros::Duration d(0.5);
 
     while ( ros::ok() )
     {
-        g_robot_state.sender.header.stamp = ros::Time::now();
+        robot_state.sender.header.stamp = ros::Time::now();
         //TODO change Task periodically
 
-        robot_state_pub.publish( g_robot_state );
+        robot_state_pub.publish( robot_state );
         ros::spinOnce();
         d.sleep();
     }
