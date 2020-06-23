6e7c729e79bcd125a3ab7390504b609e273ea35f
Travis CI <travis@travis-ci.org>
2020-06-23 02:33:27 +0200

    Doc: further fixes

--- docu/generate.sh
+++ docu/generate.sh
@@ -71,7 +71,7 @@ echo "" > .nojekyll
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-doxygen Doxyfile 2>&1 | tee doxygen.log
+doxygen Doxyfile 2>&1 doxygen.log
 
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
