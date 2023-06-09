888a9d553d31b4b4c7ea4be9bf96108f9cbfa530
Christoph Steup <steup@ovgu.de>
2023-06-09 17:50:45 +0200

    Rework hacked simple task execution launch
            - New test_robot launch file
            - Uses new test command in control node
            - Minor Cleanups

--- atwork_commander_core/src/control_node.cpp
+++ atwork_commander_core/src/control_node.cpp
@@ -27,15 +27,24 @@ enum class Command {
   GENERATE,
   STATE,
   STORE,
-  LOAD
+  LOAD,
+  TEST
 } command;
+
+enum class Result {
+  OK,
+  ERROR,
+  INVALID_ARGUMENT,
+  CONTINUE,
+  NOTHING
+};
 
 unique_ptr<Control> gControlPtr;
 string refboxName = "atwork_commander";
 vector<string> arguments;
 bool verbose = false;
 bool continous = false;
-int result = 0;
+unsigned int commandState = 0;
 
 static ostream& operator<<(ostream& os, const vector<string>& v) {
   os << "[";
@@ -54,103 +63,138 @@ static ostream& operator<<(ostream& os, Command cmd) {
     case( Command::STATE   ): return os << "\"print refbox state\"";
     case( Command::STORE   ): return os << "\"store current task\"";
     case( Command::LOAD    ): return os << "\"load current task\"";
+    case( Command::TEST    ): return os << "\"send task to robot for testing\"";
     default                 : return os << "UNKNOWN";
   };
 }
 
-static bool forward() {
+static Result forward() {
   try {
     gControlPtr->forward();
+    return Result::OK;
   }
   catch(const ControlError& e) {
     ROS_ERROR_STREAM("[REFBOX-CONTROL] Error in forwarding state occured! " << e.what());
-    return false;
+    return Result::ERROR;
   }
-  return true;
 }
 
-static bool generate() {
+static Result generate() {
   if( arguments.size() != 1 ) {
     ROS_ERROR_STREAM_NAMED("generate", "[REFBOX-CONTROL] Invalid amount of task types supplied: Expected 1: Got " << arguments.size());
-    return true;
+    return Result::INVALID_ARGUMENT;
   }
   try {
     gControlPtr->generate(arguments[0]);
+    return Result::OK;
   } catch(const ControlError& e) {
     ROS_ERROR_STREAM("[REFBOX-CONTROL] Error during task generation occured! " << e.what());
-    return false;
+    return Result::ERROR;
   }
-  return true;
 }
 
-static bool stop() {
+static Result stop() {
   try {
     gControlPtr->stop();
+    return Result::OK;
   } catch(const ControlError& e) {
     ROS_ERROR_STREAM("[REFBOX-CONTROL] Error during stopping occured! " << e.what());
-    return false;
+    return Result::ERROR;
   }
-  return true;
 }
 
-static bool start() {
+static Result start() {
   try {
     gControlPtr->start(arguments);
+    return Result::OK;
   } catch(const ControlError& e) {
     ROS_ERROR_STREAM("[REFBOX-CONTROL] Error during starting task occured! " << e.what());
-    return false;
+    return Result::ERROR;
   }
-  return true;
 }
 
-static void store() {
+static Result store() {
   if( arguments.size() != 1) {
     ROS_ERROR_STREAM_NAMED("store", "[REFBOX-CONTROL] wrong number of arguments supplied: required <fileName>");
-    result = -1;
+    return Result::INVALID_ARGUMENT;
   }
   fs::path fileName = arguments[0];
   try {
     gControlPtr->store(fileName);
+    return Result::OK;
   }
   catch(const ControlError& e) {
     ROS_ERROR_STREAM("[REFBOX-CONTROL] Error in storing task occured! " << e.what());
-    result = -1;
+    return Result::ERROR;
   }
 }
 
-static void load() {
+static Result load() {
   if( arguments.size() != 1) {
     ROS_ERROR_STREAM_NAMED("load", "[REFBOX-CONTROL] wrong number of arguments supplied: required <fileName>");
-    result = -1;
+    return Result::INVALID_ARGUMENT;
   }
   fs::path fileName = arguments[0];
   try {
     gControlPtr->load(fileName);
+    return Result::OK;
   }
   catch(const ControlError& e) {
     ROS_ERROR_STREAM("[REFBOX-CONTROL] Error in loading task occured! " << e.what());
-    result = -1;
+    return Result::ERROR;
   }
+}
+
+static Result test(unsigned int& commandState) {
+  Result temp;
+  switch(commandState) {
+    case(0):
+      ROS_DEBUG_STREAM("[REFBOX-CONTROL] Generate task for robot test.");
+      temp = generate();
+      if(temp == Result::OK) {
+        ROS_DEBUG_STREAM("[REFBOX-CONTROL] generation successfull.");
+        commandState = 1;
+        arguments.clear();
+        return Result::CONTINUE;
+      }
+      return temp;
+    case(1):
+      ROS_DEBUG_STREAM("[REFBOX-CONTROL] start task for robot test.");
+      temp = start();
+      if(temp == Result::OK) {
+        ROS_DEBUG_STREAM("[REFBOX-CONTROL] start successfull.");
+        commandState = 2;
+        return Result::CONTINUE;
+      }
+      return temp;
+    case(2):
+      ROS_DEBUG_STREAM("[REFBOX-CONTROL] start execution of task for robot test.");
+      return forward();
+    }
+    return Result::ERROR;
 }
 
 static void stateUpdate(const Control::RefboxState& state) {
   ROS_INFO_STREAM_COND_NAMED(verbose, "state", "[REFBOX-CONTROL] Current refbox state: \n\t" << state);
-  bool stopped = true;
+  Result res;
   switch( command ) {
-    case( Command::FORWARD  ): stopped = forward()  || !continous; break;
-    case( Command::STOP     ): stopped = stop()     || !continous; break;
-    case( Command::START    ): stopped = start()    || !continous; break;
-    case( Command::GENERATE ): stopped = generate() || !continous; break;
-    case( Command::STORE    ): stopped =  true;  store();    break;
-    case( Command::LOAD     ): stopped =  true;  load();     break;
-    default: stopped = false;
+    case( Command::FORWARD  ): res = forward();          break;
+    case( Command::STOP     ): res = stop();             break;
+    case( Command::START    ): res = start();            break;
+    case( Command::GENERATE ): res = generate();         break;
+    case( Command::TEST     ): res = test(commandState); break;
+    case( Command::STORE    ): res = store();            break;
+    case( Command::LOAD     ): res = load();             break;
+    default: res = Result::NOTHING;
             ROS_INFO_STREAM_NAMED("state", "[REFBOX-CONTROL] Current refbox state: \n\t" << state);
   };
-  if( stopped ) {
-    if( result == 0)
-      ROS_DEBUG_STREAM_NAMED("control", "[REFBOX-CONTROL] Command executed successfull! Shutting down!");
-    else
-      ROS_ERROR_STREAM_NAMED("control", "[REFBOX-CONTROL] Command failed! Shutting down!");
+
+  if( res == Result::OK || res == Result::CONTINUE)
+    ROS_DEBUG_STREAM_NAMED("control", "[REFBOX-CONTROL] Command executed successfull!");
+  else
+    ROS_ERROR_STREAM_NAMED("control", "[REFBOX-CONTROL] Command failed!");
+  if(continous && (res == Result::OK || res == Result::INVALID_ARGUMENT)) {
+    ROS_DEBUG_STREAM_NAMED("control", "[REFBOX-CONTROL] Shutting down!");
     ros::shutdown();
   }
 }
@@ -159,7 +203,7 @@ static bool parseArgs(int argc, char** argv) {
   po::options_description desc("control [options] command [args]\nAllowed options");
   string cmdString;
   desc.add_options()
-    ("command", po::value<string>(&cmdString), "command to execute [\"start <robots>\", \"generate <taskName>\", \"stop\", \"forward\", \"state\"]")
+    ("command", po::value<string>(&cmdString), "command to execute [\"start <robots>\", \"generate <taskName>\", \"stop\", \"forward\", \"state\", \"test <taskName>\"]")
     ("args", po::value<vector<string>>(&arguments), "command arguments")
     ("verbose,v", po::value<bool>(&verbose), "set verbosity")
     ("continous,c", po::value<bool>(&continous), "try again after failure")
@@ -179,6 +223,7 @@ static bool parseArgs(int argc, char** argv) {
   if( cmdString == "generate" ) command = Command::GENERATE;
   if( cmdString == "store"    ) command = Command::STORE;
   if( cmdString == "load"     ) command = Command::LOAD;
+  if( cmdString == "test"     ) {command = Command::TEST; continous = true;}
 
   if( command == Command::NOTHING || vm.count("help") ) {
     ROS_INFO_STREAM_NAMED("control", "[REFBOX-CONTROL] Syntax specification:\n" << desc);
@@ -219,5 +264,5 @@ int main(int argc, char** argv)
 
     cout << "Shutdown finished" << endl;
 
-    return result;
+    return 0;
 }
