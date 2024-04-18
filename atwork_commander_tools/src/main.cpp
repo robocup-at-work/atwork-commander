740c9bf9efb65843bbe1ee66709bfff9dfe114b2
Adrian Köring <adrian.koering@ovgu.de>
2024-04-18 22:24:43 +0200

    Clean up and Formatting

--- atwork_commander_tools/src/main.cpp
+++ atwork_commander_tools/src/main.cpp
@@ -1,9 +1,9 @@
-#include "ros/ros.h"
-#include "atwork_commander_msgs/ObjectTask.h"
 #include "atwork_commander_msgs/ObjectName.h"
+#include "atwork_commander_msgs/ObjectTask.h"
+#include "ros/ros.h"
 
-#include <fstream>
 #include <chrono>
+#include <fstream>
 
 const std::string FILE_OUTPUT_PATH = "/tmp/"; /**< path of the output file */
 
@@ -13,46 +13,52 @@ const std::string FILE_OUTPUT_PATH = "/tmp/"; /**< path of the output file */
  * @param[in] line data to write
  */
 void write2File(std::string filename, std::string line) {
-    std::ofstream file;
-    file.open(FILE_OUTPUT_PATH+filename, std::ios::app);
-    file << line;
-    file.close();
+  std::ofstream file;
+  file.open(FILE_OUTPUT_PATH + filename, std::ios::app);
+  file << line;
+  file.close();
 }
 
 /**
- * @brief subscriber callback which converts the message to a human readable format
- * and write a csv file
+ * @brief subscriber callback which converts the message to a human readable
+ * format and write a csv file
  * @param[in] ObjectTask message
  */
-void objectTaskCallback(const atwork_commander_msgs::ObjectTask::ConstPtr& msg) {
-    std::time_t result = std::time(nullptr); // get time for output file name
-    std::string filename(std::asctime(std::localtime(&result))); 
-    filename = "task_" + filename + ".csv";
+void objectTaskCallback(
+    const atwork_commander_msgs::ObjectTask::ConstPtr &msg) {
+  std::time_t result = std::time(nullptr); // get time for output file name
+  std::string filename(std::asctime(std::localtime(&result)));
+  filename = "task_" + filename + ".csv";
 
-    write2File(filename, "object, source, destination, placement target\n");
+  write2File(filename, "object, source, destination, placement target\n");
 
-    int numberOfSubtasks = msg->subtasks.size();
-    for(int i = 0; i < numberOfSubtasks; i++) {
-        std::string object(atwork_commander_msgs::objectName(msg->subtasks[i].object.object));
-        std::string placementTarget(atwork_commander_msgs::objectName(msg->subtasks[i].object.target));
-        ROS_INFO_STREAM(    "[TASK_OUTPUT] object " + object
-                            + " from workstation " + msg->subtasks[i].source 
-                            + " to workstation " + msg->subtasks[i].destination
-                            + (" in placement target " + placementTarget) );
-        write2File(filename, object+","+msg->subtasks[i].source+","+msg->subtasks[i].destination+","+placementTarget+"\n");
-    }
+  int numberOfSubtasks = msg->subtasks.size();
+  for (int i = 0; i < numberOfSubtasks; i++) {
+    std::string object(
+        atwork_commander_msgs::objectName(msg->subtasks[i].object.object));
+    std::string placementTarget(
+        atwork_commander_msgs::objectName(msg->subtasks[i].object.target));
+    ROS_INFO_STREAM("[TASK_OUTPUT] object " + object + " from workstation " +
+                    msg->subtasks[i].source + " to workstation " +
+                    msg->subtasks[i].destination +
+                    (" in placement target " + placementTarget));
+    write2File(filename, object + "," + msg->subtasks[i].source + "," +
+                             msg->subtasks[i].destination + "," +
+                             placementTarget + "\n");
+  }
 
-    ros::shutdown();
+  ros::shutdown();
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "task_output");
+  ros::init(argc, argv, "task_output");
 
-    ros::NodeHandle nh;
+  ros::NodeHandle nh;
 
-    ros::Subscriber subObjectTask = nh.subscribe("/atwork_commander/object_task" , 1000, objectTaskCallback);
+  ros::Subscriber subObjectTask =
+      nh.subscribe("/atwork_commander/object_task", 1000, objectTaskCallback);
 
-    ROS_INFO_STREAM("[TASK_OUTPUT] startup :)");
+  ROS_INFO_STREAM("[TASK_OUTPUT] startup :)");
 
-    ros::spin();
+  ros::spin();
 }
